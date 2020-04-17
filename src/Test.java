df08570ffcc913c52788e8ccc6e345e4d7b8fa70
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-04-17 16:25:37 +0700

    kho hieu

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+
+public class Test {
+    public static void main(String[] args) {
+        JDBCConnectionPool jdbcConnectionPool = new JDBCConnectionPool("org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb", "sa", "secret");
+        Connection connection = jdbcConnectionPool.checkOut();
+        jdbcConnectionPool.checkIn(connection);
+    }
+}
